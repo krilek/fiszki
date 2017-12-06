384a481de98bdad3c30c9a2334e5875a5cf5e86a
krilek <krilek@gmail.com>
2017-12-06 23:48:40 +0100

    basic adding word functionality, adjusted showlang for symfony needs

--- src/AppBundle/Controller/FiszkiController.php
+++ src/AppBundle/Controller/FiszkiController.php
@@ -1,5 +1,6 @@
 <?php
 namespace AppBundle\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,129 +11,201 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use AppBundle\Entity\Name;
 use AppBundle\Entity\Word;
 use AppBundle\Entity\Background;
-class FiszkiController extends Controller{
-  /**
-  * @Route("/", name="main")
-  */
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class FiszkiController extends Controller
+{
+    /**
+    * @Route("/", name="main")
+    */
   
-  public function showMain(Request $request){
-    $background = new Background();
-    $background = $background->getBackground();
-	//dobry komentarz
-    $name = new Name();
-    $form = $this->createFormBuilder($name)
+    public function showMain(Request $request)
+    {
+        $background = new Background();
+        $background = $background->getBackground();
+
+        $name = new Name();
+        $form = $this->createFormBuilder($name)
             ->add('name', TextType::class, array(
-              "attr" => array(
+                "attr" => array(
                 "class" => "input-transparent",
                 "aria-label" => "Enter name"
-              )
+                )
             ))
             ->getForm();
 
-    $form->handleRequest($request);
-
-    $templateFile;
-    $parameters;
-    $nameCookie;
-    if($request->cookies->has("name")){
-      //render main template
-      $templateFile = "main/main.html.twig";
-      $name = $request->cookies->get("name");
-      $parameters = [
+        $form->handleRequest($request);
+        $templateFile;
+        $parameters;
+        $nameCookie;
+        if ($request->cookies->has("name")) {
+            //render main template
+            $templateFile = "main/main.html.twig";
+            $name = $request->cookies->get("name");
+            $parameters = [
         "name" => $name,
         "background" => $background
       ];
-    }else if($form->isSubmitted() && $form->isValid()){
-      //create cookie and render main
-      $name = $form->getData()->name;
-      $cookieExpiration = time() + (10 * 365 * 24 * 60 * 60);
-      $nameCookie = new Cookie("name", $name, $cookieExpiration);
-      $templateFile = "main/main.html.twig";
-      $parameters = [
+        } elseif ($form->isSubmitted() && $form->isValid()) {
+            //create cookie and render main
+            $name = $form->getData()->name;
+            $cookieExpiration = time() + (10 * 365 * 24 * 60 * 60);
+            $nameCookie = new Cookie("name", $name, $cookieExpiration);
+            $templateFile = "main/main.html.twig";
+            $parameters = [
         "name" => $name,
         "background" => $background
       ];
-    }else{
-      //render beforeName template
-      $templateFile = "main/beforeName.html.twig";
-      $parameters = [
+        } else {
+            //render beforeName template
+            $templateFile = "main/beforeName.html.twig";
+            $parameters = [
         "background" => $background,
         "form" => $form->createView()
       ];
+        }
+    
+        $response = $this->render($templateFile, $parameters);
+        if (isset($nameCookie)) {
+            $response->headers->setCookie($nameCookie);
+        }
+        return $response;
     }
-    
-    $response = $this->render($templateFile,$parameters);
-    if(isset($nameCookie))
-      $response->headers->setCookie($nameCookie);
-    return $response;
-  }
-  /**
-  * @Route("/add", name="show_add_word")
-  */
-  public function showAddWord(){
-    return new Response("TESTING");
-  }
-  /**
-  * @Route("/lang/{lang}", name="show_lang")
-  */
-  public function showLangChoose(Request $request, $lang){
-    
-
-    $background = new Background();
-    $background = $background->getBackground();
-
-    /* program has to get in one round $allSWords questions from the data base, 
-    write to variable $sWords, 
-    then every one that appeared delete from array,
-    and again rand the next one */
-
-    //all Words from server;
-    $allSWords = 3;
-
-    //question to data base
-    $sWords = ['cat'=>'kot', 'sausage'=>'kielbasa', 'headphones'=>'sluchawki', 'mouse'=>'myszka'];
-
-    $actualSWord = array_rand($sWords,1);
-    
-    $_SESSION['goodWords'] = 0;
-
-    $uWord = new Word("");
-
-    $templateFile = "main/word.twig";
-    
-    $form = $this->createFormBuilder($uWord)
-    ->add('word', TextType::class)
-    ->add('save', SubmitType::class, array('label' => 'Getting word'))
-    ->getForm();
-    
-
-    $doSprawdzenia1 = $actualSWord;
-    $doSprawdzenia2 = $sWords[$actualSWord];
-    
-    $form->handleRequest($request);
-    if($form->isSubmitted()  && $form->isValid())
+    /**
+    * @Route("/add", name="show_add_word")
+    */
+    public function showAddWord(Request $request)
     {
-      $submitedWord = $form->getData()->word;
-      $uWord->setWord($submitedWord);
-      if($submitedWord == $sWords[$actualSWord])
-      {
-        $_SESSION['goodWords'] += 1;
-        unset($sWords[$actualSWord]);
-      }
-      
+        //Randomize background
+        $background = new Background();
+        $background = $background->getBackground();
+
+        //Setting up doctrine
+        $em = $this->getDoctrine()->getManager();
+
+        //Creating new object used for db queries and form creation
+        $word = new Word();
+
+        //Setup form for template
+        $transparentInput =  array(
+            "attr" => array(
+                "class" => "input-transparent",
+                "aria-label" => "Enter english word"));
+        $form = $this->createFormBuilder($word)
+        ->add('wordEn', TextType::class, $transparentInput)
+        ->add('wordPl', TextType::class, $transparentInput)
+        ->add('add', SubmitType::class, array('label' => 'Add new word'))
+        ->getForm();
+        
+        $templateFile = "newWord/newWord.html.twig";
+        $parameters = array(
+            "background" => $background,
+            "form" => $form->createView());
+        
+        //Get inputed values
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $word = $form->getData();
+            //Tell doctrine that I need to store object in db (in future)
+            $em->persist($word);
+    
+            //Execute queries
+            $em->flush();
+        }
+
+
+
+        return $this->render($templateFile, $parameters);
     }
-    $parameters = [
-      'sWord' => $actualSWord,
-      'uWord' => $uWord->getWord(),
-      'background' => $background,
-      'form' => $form->createView(),
-      'goodWords' => $_SESSION['goodWords'],
-      'HALOSPRAWDZAMZMIENNE1' =>$doSprawdzenia1,
-      'HALOSPRAWDZAMZMIENNE2' =>$doSprawdzenia2
-    ];
-    return $this->render("main/word.twig", $parameters);
-  }
-  public function cookieSet(){
-  }
+    /**
+    * @Route("/lang/{lang}", name="show_lang")
+    */
+    public function showLangChoose(Request $request, $lang, SessionInterface $session)
+    {
+        $session->start();
+
+
+        //Background randomize
+        $background = new Background();
+        $background = $background->getBackground();
+
+        /* program has to get in one round $allSWords questions from the data base,
+        write to variable $sWords,
+        then every one that appeared delete from array,
+        and again rand the next one */
+
+        //question to data base
+        // $sWords = [];
+
+        // $actualSWord = array_rand($sWords, 1);
+    
+        // $_SESSION['goodWords'] = 0;
+        //Migrated to symfony session
+        $session->set('goodWords', 0);
+        //Here I will add some data pulling from db
+        //For now dummy classes which you should use maybe
+        $word = new Word("english", "angielski");
+        $word->id = 1;
+        /*
+        DUMMY DATA
+        */
+        $w2 = new Word('cat', 'kot');
+        $w2->id = 2;
+        $w3 = new Word('sausage', 'kielbasa');
+        $w3->id = 3;
+        $w4 = new Word('headphones', 'sluchawki');
+        $w4->id = 4;
+        $w5 = new Word('mouse', 'myszka');
+        $w5->id = 5;
+        /*
+            END OF DUMMY DATA
+        */
+        //Prepare form and template
+        $templateFile = "learn/word.html.twig";
+        $transparentInput =  array(
+            "attr" => array(
+                "class" => "input-transparent",
+                "aria-label" => "Enter english word"));
+        $form = $this->createFormBuilder($word)
+            ->add('word'.ucfirst($lang), TextType::class, $transparentInput)
+            ->add('save', SubmitType::class, array('label' => 'Check answer'))
+            ->getForm();
+    
+        //Get user data
+        $form->handleRequest($request);
+        $submitedWord = "";
+        if ($form->isSubmitted()  && $form->isValid()) {
+            $submitedWord = $form->getData();
+            //           \/ - word from form
+            // New if ($submitedWord->getWordEn() == $savedWord or id in something){
+                // $counter = $session->get('goodWords') + 1;
+                // $session->set('goodWords', $counter);
+            // }
+            // if ($submitedWord == $sWords[$actualSWord]) {
+            //     $_SESSION['goodWords'] += 1;
+            //     unset($sWords[$actualSWord]);
+            // }
+        }
+        //Preparing parameters
+        $parameters = [
+            'sWord' => $word->getWordEn(),
+            'w' => $submitedWord,
+            'background' => $background,
+            'form' => $form->createView(),
+            'goodWords' => $session->get('goodWords'),
+            'lang' => $lang
+            // 'sWord' => $actualSWord,
+            // 'uWord' => $uWord->getWord(),
+            // 'HALOSPRAWDZAMZMIENNE1' =>$doSprawdzenia1,
+            // 'HALOSPRAWDZAMZMIENNE2' =>$doSprawdzenia2
+        ];
+        if ($lang == 'pl') {
+            $parameters['sWord'] = $word->getWordPl();
+        } elseif ($lang == 'en') {
+            $parameters['sWord'] = $word->getWordEn();
+        }
+        return $this->render($templateFile, $parameters);
+    }
 }
-?>
--- src/AppBundle/Entity/Word.php
+++ src/AppBundle/Entity/Word.php
@@ -1,17 +1,57 @@
 <?php
+/**
+ * MyClass File Doc Comment
+ *
+ * @category MyClass
+ * @package  MyPackage
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.hashbangcode.com/
+ *
+ */
 namespace AppBundle\Entity;
-class Word{
-  public $word;
-  public function __construct($word = null){
-    if($word != null)
-      $this->setword($word);
-  }
-  public function setWord($word){
-    $this->word = $word;
-  }
-  public function getWord(){
-    return $this->word;
-  }
-}
 
-?>
+/**
+ * MyClass Class Doc Comment
+ *
+ * @category Class
+ * @package  Fiszki
+ * @author   krilek, Vengard
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/krilek/fiszki
+ */
+
+class Word
+{
+    //FIXME: Change id to private when working on db, changed temporarily
+    public $_id;
+    private $_wordEn;
+    private $_wordPl;
+
+    public function __construct($wordEn = null, $wordPl = null)
+    {
+        if ($wordEn != null && $wordPl != null) {
+            $this->_wordEn = $wordEn;
+            $this->_wordPl = $wordPl;
+        }
+    }
+    public function getId()
+    {
+        return $this->_id;
+    }
+    public function getWordEn()
+    {
+        return $this->_wordEn;
+    }
+    public function getWordPl()
+    {
+        return $this->_wordPl;
+    }
+    public function setWordPl($word)
+    {
+        $this->_wordPl = $word;
+    }
+    public function setWordEn($word)
+    {
+        $this->_wordEn = $word;
+    }
+}
